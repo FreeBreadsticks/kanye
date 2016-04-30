945d0b8802848880ab13f46d7f4010b5ebf0bbd7
Erika <erika.hughes@dbspublishing.com>
2016-04-30 00:54:58 +0000

    install devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Splurty::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -26,4 +27,6 @@ Splurty::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
